b8726844ef0821aaa1cd9b2f90245810ff636d1d
nahmed <Nasim.Ahmed@melroseinteriors.co.uk>
2025-01-24 14:51:47 +0000

    set up tail wind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import './App.css'
-
+import Hero from './Components/Hero/Hero'
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <Hero/>
     </>  
   )
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import "./Hero.css";
+
+function Hero() {
+    return (
+    <>
+            <div>
+                <h1>Hello World</h1>
+            </div>
+    </>
+    )
+}
+
+export default Hero
